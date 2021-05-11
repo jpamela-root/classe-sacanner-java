d05fcb2faf26c5c613b9ce7e24c328d90af0d43b
jpamela_root <59771746+jpamela-root@users.noreply.github.com>
2021-05-11 19:44:24 -0300

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Qual o seu nome: ");
+        String nome = scanner.next();
+        System.out.println("Seja bem vindo " + nome + "!");
+    }
+}
+
+
+
+
+
+
+
+
+
